e39cc3e70c71becbf391523b07ab9073c9438000
tewat <hollywoodstreets23@gmail.com>
2022-05-08 14:04:04 +0300

    Working HEAD for /index.html

--- src/jvmMain/kotlin/server.kt
+++ src/jvmMain/kotlin/server.kt
@@ -1,21 +1,21 @@
-import io.ktor.application.call
+import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.html.respondHtml
-import io.ktor.http.HttpStatusCode
-import io.ktor.routing.get
-import io.ktor.routing.routing
+import io.ktor.http.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.routing.*
 import kotlinx.html.*
 
 fun HTML.index() {
     head {
-        title("Hello from Ktor!")
+        title("Hello from Server!")
     }
     body {
         div {
-            +"Hello from Ktor"
+            +"Hello from Server"
         }
         div {
             id = "root"
@@ -26,9 +26,10 @@ fun HTML.index() {
 
 fun main() {
     embeddedServer(Netty, port = 9090, host = "127.0.0.1") {
+        install(AutoHeadResponse)
         routing {
-            get("/") {
-                call.respondHtml(HttpStatusCode.OK, HTML::index)
+            get("/index.html") {
+                call.respondHtml(HttpStatusCode.OK) { index() }
             }
             static("/static") {
                 resources()
